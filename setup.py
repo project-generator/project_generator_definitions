c50b1f515174c87e269acd4cdce49be7c239c8a8
Martin Kojtal <c0170@rocketmail.com>
2015-12-29 12:43:17 +0100

    Setup - v0.2.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='project_generator_definitions',
-    version='0.2.2',
+    version='0.2.3',
     description='Project generator definitions for tools',
     author='Martin Kojtal',
     author_email='c0170@rocketmail.com',
