eb4852b8aff3f1ec4eb118dbe910fd6647df2020
Martin Kojtal <c0170@rocketmail.com>
2016-07-11 09:35:58 +0100

    Setup v0.2.26

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='project_generator_definitions',
-    version='0.2.25',
+    version='0.2.26',
     author_email='c0170@rocketmail.com',
     keywords="definitions mcu uvision iar coide",
     url="https://github.com/project-generator/project_generator_definitions",
