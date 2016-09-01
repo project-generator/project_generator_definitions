5657637d2fa538e37bdf1b7087c4b7ca9dac11f7
Kevin Gillespie <kgills@gmail.com>
2016-09-01 13:03:05 -0500

    [MAX32620] Adding max32620mbed.

--- project_generator_definitions/target/targets.py
+++ project_generator_definitions/target/targets.py
@@ -187,6 +187,9 @@ PROGENDEF_TARGETS = {
     'max32620hsp': {
         'mcu':'mcu/maxim/max32620',
     },
+    'max32620mbed': {
+        'mcu':'mcu/maxim/max32620',
+    },
     'mbed-lpc11u24': {
         'mcu':'mcu/nxp/lpc11u24_201',
     },
